a35fb3c57e34bc785f6cafdc9768616587fd6c5e
khomboljuice <abz619ygm@gmail.com>
2024-03-16 01:39:50 +0000

    [ADDS] Initial population for db tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +13,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
+        $user = User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        Post::factory()->create([
+            'user_id' => $user->id,
+            'post_content' => 'This is a test post.',
+        ]);
     }
 }
+
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
-    return view('index');
+    return redirect()->route('posts.index');
 });
 
 Route::resource('posts', PostController::class);
